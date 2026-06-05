4ab7cc52044ad6f86883701339a61aa265999e65
Kusuma y <kusumayogesh209@gmail.com>
2026-06-05 12:58:15 +0530

    Delete mealmate/delivery directory

--- mealmate/delivery/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.hello, name = 'hello'),
-    path('open_signup', views.open_signup,  name = 'open_signup'),
-    path('open_signin', views.open_signin,  name = 'open_signin'),
-    path('signup', views.signup,  name = 'signup'),
-]
